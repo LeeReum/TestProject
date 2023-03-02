f46d7efbb375d7fbf36ce99dde3346cc25e7eb2e
hs Kim <hs2014kim@naver.com>
2023-03-02 22:09:50 +0900

    second commit

--- image move/Program.cs
+++ image move/Program.cs
@@ -241,7 +241,10 @@ namespace image_move
                 {
                     Console.WriteLine(exception.GetType());
                     Console.WriteLine("while문에서 오류발생");
-                    start = false;  //asd
+                    start = false;
+
+
+                    int asd=0;
                 }
             }
         }
